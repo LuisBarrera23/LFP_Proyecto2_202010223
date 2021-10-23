d5088260dc3ff569a92af19faaa86791c0e9b4f3
LuisBarrera23 <78067073+LuisBarrera23@users.noreply.github.com>
2021-10-23 02:03:15 -0600

    text area

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from tkinter import Button,Tk, ttk,filedialog,messagebox,Label
+from tkinter import Button, Text,Tk, ttk,filedialog,messagebox,Label,Scrollbar
+from tkinter import *
 
 #variables globales
 ventana=Tk()
+textE=Text()
+textS=Text()
 def generarVentana():
-    global ventana,canvas,combo,label
-    label=Label(ventana, width=135, height=40)
-    label.place(x=220,y=80)
+    global ventana,textE,textS
     ventana.configure(background="#008080")
-    ancho=1200
+    ancho=1400
     alto=700
     x=ventana.winfo_screenwidth()
     #calculamos la coordenada X donde se posicionara la ventana
@@ -18,7 +19,27 @@ def generarVentana():
     ventana.geometry('%dx%d+%d+%d' % (ancho, alto, x, y))
     ventana.title("Proyecto 2")
 
+
+    frameEntrada=Frame(ventana)
+    frameEntrada.place(x=200,y=80)
+    scrollbar1=Scrollbar(frameEntrada, orient='horizontal')
+    scrollbar1.pack(side=BOTTOM, fill='x')
+    textE=Text(frameEntrada, font=("Calibri, 14"), wrap="none", xscrollcommand=scrollbar1.set,width=58,height=25,state=DISABLED)
+    textE.pack()
+    scrollbar1.config(command=textE.xview)
+
+    frameSalida=Frame(ventana)
+    frameSalida.place(x=900,y=80)
+    scrollbar2=Scrollbar(frameSalida, orient='horizontal')
+    scrollbar2.pack(side=BOTTOM, fill='x')
+    textS=Text(frameSalida, font=("Calibri, 14"),background="#696969", wrap="none", xscrollcommand=scrollbar2.set,width=43,height=25)
+    textS.pack()
+    scrollbar2.config(command=textS.xview)
+    
+
+
     ventana.mainloop()
+
 
 if __name__=='__main__':
     generarVentana()
